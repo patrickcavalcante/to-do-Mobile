315f28df372c235a810e64075c4dca7fc4310408
patrickclima2 <patrickc.lima2@gmail.com>
2021-12-21 18:50:42 -0300

    created function creat update delet and done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,17 +7,32 @@ import { TodoInput } from '../components/TodoInput';
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTaskItem = {
+      id: Math.random(),
+      title: newTaskTitle,
+      done: false,
+    }
+
+    setTasks(oldstate => [...oldstate, newTaskItem]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updateTasks = tasks.map(task => ({... task}));
+
+    const foundItem = updateTasks.find(item => item.id === id);
+    if(!foundItem) return
+
+    foundItem.done = !foundItem.done;
+    setTasks(updateTasks)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const filterId = tasks.filter(tasks => tasks.id !== id);
+    setTasks(filterId);
   }
 
   return (
